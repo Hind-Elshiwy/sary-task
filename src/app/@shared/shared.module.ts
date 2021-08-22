1087b012ed78ccfe1054523f22100408fdd0a28f
hind <hend@vapulus.com>
2021-08-22 19:51:25 +0200

    adding 404 component and internet connection component

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InternetConnectionComponent } from './internet-connection/internet-connection.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule
   ],
   declarations: [
     InternetConnectionComponent,
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConnectionService } from 'ng-connection-service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sary';
+  status = 'ONLINE';
+  isConnected = true;
+ 
+  constructor(
+    private connectionService: ConnectionService,
+    private router: Router,
+    ) {
+    this.connectionService.monitor().subscribe(isConnected => {
+      this.isConnected = isConnected;
+      if (this.isConnected) {
+        this.status = "ONLINE";
+        this.router.navigate(['/home'])
+
+      }
+      else {
+        this.status = "OFFLINE";
+        this.router.navigate(['/internet-connection'])
+      }
+    })
+  }
 }
